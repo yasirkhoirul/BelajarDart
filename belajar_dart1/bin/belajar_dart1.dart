de59309f4a0aa53fb8799689499e2efd45f0cc06
yasirkhoirul <yasirkhoirul9@gmail.com>
2025-08-19 20:57:43 +0700

    belajar dengan variabel const dan final

---
+++ belajar_dart1/bin/belajar_dart1.dart
@@ -0,0 +1,12 @@
+import 'package:belajar_dart1/belajar_dart1.dart' as belajar_dart1;
+
+void main(List<String> arguments) {
+
+  var hasilcalculate = belajar_dart1.calculate();
+  //belajar menggunakan const
+  const phi = 3.14;
+  final r = 7;
+  final hasil = phi*r*r;
+  print("hasil luas dari lingkarang dengan r  7 adalah $hasil");
+  print('Hello world: ini memanggil fungsi calculate dan menampilkannya hasilnya adalah = $hasilcalculate!');
+}
