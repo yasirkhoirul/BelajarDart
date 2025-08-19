da98c2fe8ad3f314e3e6b1e5b03baa0cf32ac12d
yasirkhoirul <yasirkhoirul9@gmail.com>
2025-08-19 21:06:21 +0700

    menambahkan program input

---
+++ belajar_dart1/bin/tryProgramInput.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(){
+  stdout.write("silahkan masukkan nama anda :");
+  final name = stdin.readLineSync()!;
+  stdout.write("masukkan umur anda :");
+  final umur = stdin.readLineSync()!;
+  print("nama anda adalah $name dan umur anda adalah $umur");
+
+}
