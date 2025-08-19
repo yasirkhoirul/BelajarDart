b3386a4b42fd96652418f0c9bed2975a8aec23da
yasirkhoirul <yasirkhoirul9@gmail.com>
2025-08-20 02:53:47 +0700

    mengupdate program input

--- belajar_dart1/bin/tryProgramInput.dart
+++ belajar_dart1/bin/tryProgramInput.dart
@@ -4,7 +4,7 @@ void main(){
   stdout.write("silahkan masukkan nama anda :");
   final name = stdin.readLineSync()!;
   stdout.write("masukkan umur anda :");
-  final umur = stdin.readLineSync()!;
+  final umur = int.parse(stdin.readLineSync()!);
   print("nama anda adalah $name dan umur anda adalah $umur");
 
 }
